a27e2962be0217d75ecbc1b9c6e70098782e8213
herdeybayor <shereefdeen4real@gmail.com>
2024-09-28 12:27:50 +0100

    feat: Add axios, install, and lucide-react dependencies
    This commit adds the axios, install, and lucide-react dependencies to the package.json and pnpm-lock.yaml files. These dependencies are required for the application to function properly.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,56 @@
+import { useCallback, useEffect, useState } from "react";
+import { Loader2 } from "lucide-react";
+
+const baseUrl = "https://jsonplaceholder.typicode.com";
+
 function App() {
+    const [loading, setLoading] = useState(false);
+    const [errorMessage, setErrorMessage] = useState("");
+    const [users, setUsers] = useState([]);
+
+    const fetchPhotos = useCallback(async (baseUrl: string) => {
+        try {
+            setLoading(true);
+            // await new Promise((resolve) => setTimeout(resolve, 3000));
+            const res = await fetch(`${baseUrl}/users`);
+            const data = await res.json();
+            setUsers(data);
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        } catch (error: any) {
+            console.log({ error });
+            setErrorMessage(error?.message || "An error occurred");
+        } finally {
+            setLoading(false);
+        }
+    }, []);
+
+    useEffect(() => {
+        fetchPhotos(baseUrl);
+    }, [fetchPhotos]);
+
+    if (loading) {
+        return (
+            <div className="h-screen bg-pink-500 text-white flex items-center justify-center">
+                <Loader2 size={120} className="animate-spin" />
+            </div>
+        );
+    }
+
+    if (errorMessage) {
+        return (
+            <div className="h-screen bg-pink-500 text-white flex items-center justify-center">
+                <p className="font-bold text-3xl text-center">{errorMessage}</p>
+            </div>
+        );
+    }
     return (
         <div>
-            <h1 className="text-3xl font-bold underline">Hello world!</h1>
+            <h1 className="text-3xl font-bold underline">Users</h1>
+            <ul>
+                {users.map((user: { id: number; name: string }) => (
+                    <li key={user.id}>{user.name}</li>
+                ))}
+            </ul>
         </div>
     );
 }
